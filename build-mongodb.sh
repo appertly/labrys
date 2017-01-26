68566222837b8df89af946c49d1b0293aa5f9b38
Jonathan Hawk <jonathan@hawk.ninja>
2017-01-26 15:46:04 -0500

    Maing travis happy

--- build-mongodb.sh
+++ build-mongodb.sh
@@ -2,9 +2,9 @@
 mkdir -p /tmp/builds/hippo
 sudo apt-get install -y --no-install-recommends libtool make wget hhvm-dev libdouble-conversion-dev liblz4-dev
 cd /tmp/builds/hippo
-wget -O /tmp/builds/hippo/mongo.tgz https://github.com/mongodb/mongo-hhvm-driver/releases/download/1.2.0/hhvm-mongodb-1.2.0.tgz
+wget -O /tmp/builds/hippo/mongo.tgz https://github.com/mongodb/mongo-hhvm-driver/releases/download/1.2.2/hhvm-mongodb-1.2.2.tgz
 tar -xzf mongo.tgz
-cd hhvm-mongodb-1.2.0
+cd hhvm-mongodb-1.2.2
 hphpize
 cmake .
 make configlib
